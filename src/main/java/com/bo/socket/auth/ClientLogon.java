1518272fdeae52c90ae3d322407717cf12e3f5c3
krischou <krisdevchou@gmail.com>
2021-07-14 10:50:37 +0800

    finish ClientLogon

--- src/main/java/com/bo/socket/auth/ClientLogon.java
+++ src/main/java/com/bo/socket/auth/ClientLogon.java
@@ -25,18 +25,18 @@ public class ClientLogon extends Message {
             LogonType = 1;
             Account = 100700;
             TwoFA = "1F6A".getBytes();
-            UserName = "BOU7".toCharArray();
+            UserName = "BOU7".getBytes();
             TradingSessionID = 506;
-            PrimaryOrderEntryIP = "1".toCharArray();
-            SecondaryOrderEntryIP = "1".toCharArray();
-            PrimaryMarketDataIP = "1".toCharArray();
-            SecondaryMarketDataIP = "1".toCharArray();
-            SendingTime = "".toCharArray();
+            PrimaryOrderEntryIP = "1".getBytes();
+            SecondaryOrderEntryIP = "1".getBytes();
+            PrimaryMarketDataIP = "1".getBytes();
+            SecondaryMarketDataIP = "1".getBytes();
+            SendingTime = "".getBytes();
             MsgSeqNum = 1500201;
             Key = 432451;
             LoginStatus = 0;
             RejectReason = 0;
-            RiskMaster = Character.MIN_VALUE;
+            RiskMaster = 0;
             
             ByteBuffer buffer = ByteBuffer.allocate(MessageLen);
             buffer.put(Data1)
@@ -45,7 +45,18 @@ public class ClientLogon extends Message {
                 .putShort(LogonType)
                 .putInt(Account)
                 .put(TwoFA).position(16)
-                .put(toBytes(UserName)).position(22);
+                .put(UserName).position(22)
+                .putInt(TradingSessionID)
+                .put(PrimaryOrderEntryIP).position(50)
+                .put(SecondaryOrderEntryIP).position(74)
+                .put(PrimaryMarketDataIP).position(98)
+                .put(SecondaryMarketDataIP).position(122)
+                .put(SendingTime).position(130)
+                .putInt(MsgSeqNum)
+                .putInt(Key)
+                .putShort(LoginStatus)
+                .putShort(RejectReason)
+                .put(RiskMaster);
 
             buffer.position(0);
             WritableByteChannel channel = Channels.newChannel(out);
@@ -79,6 +90,18 @@ public class ClientLogon extends Message {
             LogonType = byteBuffer.getShort();
             Account = byteBuffer.getInt();
             byteBuffer.get(TwoFA);
+            byteBuffer.get(UserName);
+            TradingSessionID = byteBuffer.getInt();
+            byteBuffer.get(PrimaryOrderEntryIP);
+            byteBuffer.get(SecondaryOrderEntryIP);
+            byteBuffer.get(PrimaryMarketDataIP);
+            byteBuffer.get(SecondaryMarketDataIP);
+            byteBuffer.get(SendingTime);
+            MsgSeqNum = byteBuffer.getInt();
+            Key = byteBuffer.getInt();
+            LoginStatus = byteBuffer.getShort();
+            RejectReason = byteBuffer.getShort();
+            RiskMaster = byteBuffer.get();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -88,5 +111,17 @@ public class ClientLogon extends Message {
         System.out.println("LogonType: " + LogonType);
         System.out.println("Account: " + Account);
         System.out.println("TwoFA: " + new String(TwoFA));
+        System.out.println("UserName: " + new String(UserName));
+        System.out.println("TradingSessionID: " + TradingSessionID);
+        System.out.println("PrimaryOrderEntryIP: " + new String(PrimaryOrderEntryIP));
+        System.out.println("SecondaryOrderEntryIP: " + new String(SecondaryOrderEntryIP));
+        System.out.println("PrimaryMarketDataIP: " + new String(PrimaryMarketDataIP));
+        System.out.println("SecondaryMarketDataIP: " + new String(SecondaryMarketDataIP));
+        System.out.println("SendingTime: " + new String(SendingTime));
+        System.out.println("MsgSeqNum: " + MsgSeqNum);
+        System.out.println("Key: " + Key);
+        System.out.println("LoginStatus: " + LoginStatus);
+        System.out.println("RejectReason: " + RejectReason);
+        System.out.println("RiskMaster: " + RiskMaster);
     }
 }
